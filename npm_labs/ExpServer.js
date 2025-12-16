3820c14c397d32beb68b3367eb43af0cb8a111eb
Mikki931 <mikki0931@gmail.com>
2025-12-16 11:23:12 +0530

    just added new methods

--- npm_labs/ExpServer.js
+++ npm_labs/ExpServer.js
@@ -2,12 +2,23 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/users/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/user", (req, res) => {
+  res.send(req.query);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.post("/", (req, res) => {
-  res.send("Hello World Post");
+  const { name, email, password } = req.body;
+  res.send(`Name: ${name}  Email:${email}   Password:${password}`);
 });
 
 app.listen(3000, () => {
